1a06fa8e3cc426a4fb55d6b06bff55c974cbe0ec
Chiller132196 <2580911814@qq.com>
2024-10-14 17:28:42 +0800

    蝙蝠敌怪逻辑，自动归位脚本

--- Assets/Scripts/Platform/AntiFloat.cs
+++ Assets/Scripts/Platform/AntiFloat.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AntiFloat : MonoBehaviour
 {
     public Rigidbody rb;
 
+    private Vector3 originPos;
+
+    public float limitOffset = 50.0f;
+
+    private float offset = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originPos = transform.position;
     }
 
     // Update is called once per frame
@@ -22,18 +30,29 @@ public class AntiFloat : MonoBehaviour
     {
         if (rb.velocity.y > 0)
         {
-            AntiFloatForce();
+
         }
         else
         {
-            StopForce();
+
         }
 
+        offset = transform.position.y - originPos.y;
+
+
+        if (offset > 5)
+        {
+            rb.AddForce(Vector3.down * (120.0f + offset * 10));
+        }
+        else if (offset < (-1 * limitOffset))
+        {
+            rb.AddForce(Vector3.up * 48.0f);
+        }
     }
 
     void AntiFloatForce()
     {
-        rb.drag = rb.velocity.magnitude;
+        rb.drag = rb.velocity.magnitude * 2 + 5;
     }
 
     void StopForce()
